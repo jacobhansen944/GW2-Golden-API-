8873533ac507333a50dd5a539c2d62b9f97644e0
jacobhansen944 <30838453+jacobhansen944@users.noreply.github.com>
2017-08-15 11:52:08 -0400

    Add files via upload

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,7 @@
+from time import localtime
+
 print "Hello World"
+
 
 # what would this GW2 APi actually need to do?
 #
@@ -6,26 +9,43 @@ print "Hello World"
 # filter results by time, by item, by flipping value
 # basic interface
 # saves the key in a .txt file so that it can be loaded from memory, not entered every run
+# output file will be titled by the day and time, like trades8-22-830pm
 #
 #
 #
 #
+
+
+# fileName = raw_input("Enter the name of the file to be read: ")
 #
+# fileObjectRead = open(fileName, "r")
+#
+# fileContents = fileObjectRead.read()
+#
+# print fileContents
+#
+# fileObjectRead.close()
 
 
-fileName = raw_input("Enter the name of the file to be read: ")
+# fileObjectWrite = open(fileName, "w")
+#
+# fileObjectWrite.write(fileContents + raw_input("Now enter some text to add to the file: "))
+#
+# fileObjectWrite.close()
 
-fileObjectRead = open(fileName, "r")
+nowTime = localtime()
 
-fileContents = fileObjectRead.read()
+print nowTime
 
-print fileContents
+# month-day-time
+formattedTime = str(nowTime[1]) + "-" + str(nowTime[2]) + "-" + str(nowTime[3]) + str(nowTime[4])
 
-fileObjectRead.close()
+print formattedTime
 
+fileObjectWrite = open(formattedTime, "w")
 
-fileObjectWrite = open(fileName, "w")
-
-fileObjectWrite.write(fileContents + raw_input("Now enter some text to add to the file: "))
+fileObjectWrite.write(formattedTime)
 
 fileObjectWrite.close()
+
+# write a filename containing the current timestamp
